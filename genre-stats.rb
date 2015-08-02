c44473bd962b42d9031575296680c259758210d4
Hanna Reitz <xanclic@xanclic.moe>
2015-08-02 22:53:04 +0200

    Classify studios, staff, and age rating, too

--- genre-stats.rb
+++ genre-stats.rb
@@ -8,6 +8,30 @@ require 'uri'
 
 $db = nil
 
+PREFIX_WEIGHTS = {
+    'Genre'             => 1.0,
+    'Studio'            => 0.3,
+    'Classification'    => 0.1,
+    'Staff'             => 0.1,
+    'Staff: Director'   => 1.0,
+    'Staff: Script'     => 1.0,
+    'Staff: Storyboard' => 0.7,
+    'Staff: Screenplay' => 0.7,
+    'Staff: Music'      => 1.0,
+    'Staff: Sound Director' => 0.5,
+    'Staff: Art Director'   => 1.0,
+    'Staff: Key Animation'  => 0.3,
+    'Staff: Episode Director'   => 1.0,
+    'Staff: Animation Director' => 0.3,
+    'Staff: Character Design'   => 0.7,
+    'Staff: Series Composition' => 0.3,
+    'Staff: Original Creator'   => 0.4,
+    'Staff: Special Effects'    => 0.2,
+    'Staff: Theme Song Composition'     => 0.3,
+    'Staff: Original Character Design'  => 0.7,
+    'Staff: Chief Animation Director'   => 0.2
+}
+
 
 def die(msg)
     $stderr.puts(msg)
@@ -68,19 +92,89 @@ class Array
 end
 
 
+def simplified_role(role)
+    if role.include?('(')
+        i = 0
+        i += 1 until role[i] == '('
+        si = i
+        nesting = 1
+        while nesting > 0 && role[i]
+            i += 1
+            nesting += 1 if role[i] == '('
+            nesting -= 1 if role[i] == ')'
+        end
+        s = role[0..(si-1)]
+        e = role[(i+1)..-1]
+        s = '' unless s
+        e = '' unless e
+        role = simplified_role(s.strip + ' ' + e.strip)
+    end
+    role.strip
+end
+
+
+def expanded_genres(anime, main_studio_handling)
+    genres = anime['genres'].map { |g|
+        "Genre: #{g}"
+    } + anime['studio'].map { |s|
+        if s['main_studio']
+            if main_studio_handling == :double
+                ["Studio: #{s['studio_name']}", "Studio: #{s['studio_name']} (main)"]
+            elsif main_studio_handling == :extra
+                ["Studio: #{s['studio_name']} (main)"]
+            else
+                ["Studio: #{s['studio_name']}"]
+            end
+        else
+            ["Studio: #{s['studio_name']}"]
+        end
+    }.flatten + anime['staff'].map { |s|
+        roles = (s['role'] ? simplified_role(s['role']) : '').split(',').map { |r| r.strip }
+        if s['name_last'].empty? && s['name_first'].empty?
+            roles = []
+            name = nil
+        elsif s['name_last'].empty?
+            name = s['name_first']
+        elsif s['name_first'].empty?
+            name = s['name_last']
+        else
+            name = "#{s['name_last']}, #{s['name_first']}"
+        end
+        roles.map { |r|
+            if r.empty?
+                "Staff: #{name}"
+            else
+                "Staff: #{r}: #{name}"
+            end
+        }
+    }.flatten
+    genres << "Classification: #{anime['classification']}"
+
+    return genres
+end
+
+
 def fetch_genres(user, sess)
     completed_list = sess.get("user/#{user}/animelist")['lists']['completed']
 
     genre_votes = {}
 
-    completed_list.each do |a|
-        anime = sess.get("anime/#{a['anime']['id']}")
+    i = 0
+    n = completed_list.length
 
-        anime['genres'].each do |genre|
+    completed_list.each do |a|
+        anime = sess.get("anime/#{a['anime']['id']}/page")
+
+        expanded_genres(anime, :duplicate).each do |genre|
             genre_votes[genre] = [] unless genre_votes[genre]
             genre_votes[genre] << a['score'].to_f
         end
+
+        i += 1
+        print "#{i}/#{n}\r"
+        $stdout.flush
     end
+    puts
 
     genre_votes.map { |g, v|
         [ g, v.length, v.average, v.sd ]
@@ -128,7 +222,7 @@ end
 
 sess = AnilistSession.new($db['client-id'], $db['client-secret']) unless commands[0] == 'help'
 
-if commands[0] != 'help' && (!$db['genres'] || user_changed)
+if commands[0] != 'help' && (!$db['genres'] || user_changed || options['refresh'])
     $db['genres'] = fetch_genres($db['user'], sess)
 end
 
@@ -142,34 +236,83 @@ elsif commands[0] == 'recommend'
 
     weighted_genres = {}
     $db['genres'].each do |gv|
+        prefix = /^[^:]+/.match(gv[0])
+        prefix_weight = PREFIX_WEIGHTS[prefix[0]]
+        prefix_weight = 1.0 unless prefix_weight
+        if prefix == 'Staff'
+            prefix = /^[^:]+:[^:]+/.match(gv[0])
+            prefix_weight = PREFIX_WEIGHTS[prefix[0]] if PREFIX_WEIGHTS[prefix[0]]
+        end
+
         weighted_sd = gv[3] + 4.0 / gv[1]
+        weighted_sd /= prefix_weight
         weighted_genres[gv[0]] = [gv[2], 1.0 / (1.0 + weighted_sd)]
     end
 
-    list = sess.get('browse/anime', { year: year, season: season })
-    list.map! { |a|
-        full = sess.get("anime/#{a['id']}")
+    list = sess.get('browse/anime', { year: year, season: season, full_page: true })
 
-        gm = full['genres'].map { |g| weighted_genres[g] }.select { |ws| ws }
+    i = 0
+    n = list.length
+
+    list.map! { |a|
+        full = sess.get("anime/#{a['id']}/page")
+
+        gm = expanded_genres(full, :extra).map { |g| weighted_genres[g] }.select { |ws| ws }
         if gm.empty?
             calc_score = -Float::INFINITY
+            weight = 0.0
+            calculation = nil
         else
-            calc_score = gm.map { |ws| ws[0] * ws[1] }.inject(:+) / gm.map { |ws| ws[1] }.inject(:+)
+            weight = gm.map { |ws| ws[1] }.inject(:+)
+            calc_score = gm.map { |ws| ws[0] * ws[1] }.inject(:+) / weight
+            calculation = expanded_genres(full, :extra).map { |g|
+                [g, weighted_genres[g]]
+            }.select { |ws| ws[1] }.sort { |ws1, ws2|
+                ws2[1][1] <=> ws1[1][1]
+            }.map { |ws|
+                "#{ws[0]} (%.2f * %.2f)" % [ws[1][0], ws[1][1]]
+            } * ', '
+        end
+
+        i += 1
+        print "#{i}/#{n}\r"
+        $stdout.flush
+
+        title = "#{a['title_romaji']}"
+        if a['title_romaji'] != a['title_english']
+            title += " (#{a['title_english']})"
         end
 
         { id:           a['id'],
-          title:        a['title_english'],
+          title:        title,
           avg_score:    a['average_score'],
           eps:          a['total_episodes'],
-          calc_score:   calc_score }
+          calc_score:   calc_score,
+          weight:       weight,
+          calculation:  calculation }
     }
+    puts
+
     list.sort! { |x, y|
         y[:calc_score] <=> x[:calc_score]
     }
 
-    puts list.map { |a|
-        "#{a[:title]} (#{a[:id]}, #{a[:eps]} episodes): Avg. score %.2f, calc. score %.2f" % [a[:avg_score], a[:calc_score]]
-    } * "\n"
+    weight_classes = []
+    wc = 0
+    until list.empty?
+        weight_classes[wc] = list.select { |a| a[:weight] < wc + 1 }
+        list -= weight_classes[wc]
+        wc += 1
+    end
+
+    weight_classes.map.with_index { |wco, i| i }.reverse.each do |wc|
+        puts
+        puts "=== weight class #{wc}+ ==="
+        puts
+        puts weight_classes[wc].map { |a|
+            "- C %.2f (w %.1f), A %.2f: #{a[:title]} (#{a[:id]}, #{a[:eps]} episodes)\n  #{a[:calculation]}" % [a[:calc_score], a[:weight], a[:avg_score]]
+        } * "\n"
+    end
 elsif commands[0] == 'help'
     puts 'Global options:'
     puts ' --client-id=...'
